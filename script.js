a95333b6e61604198fcdc40b26a6b1920083184b
ksuds08 <65447814+ksuds08@users.noreply.github.com>
2025-07-23 11:02:19 -0700

    Add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('ResumeBot is ready to assist you!');
+    // Future JavaScript functionality for chatbot and template generation will go here.
+});
